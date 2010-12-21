797bff8842f23ee1bff34a35ed4906abf86de2f7
bolhoso <lucasrosada@gmail.com>
2010-12-20 22:34:38 -0200

    The persister and outputStreamName have been added

--- speech01/src/main/java/io/s4/example/speech01/SentenceReceiverPE.java
+++ speech01/src/main/java/io/s4/example/speech01/SentenceReceiverPE.java
@@ -1,11 +1,45 @@
 package io.s4.example.speech01;
 
+import io.s4.dispatcher.Dispatcher;
 import io.s4.processor.AbstractPE;
 
 public class SentenceReceiverPE extends AbstractPE {
 
+    private Dispatcher dispatcher;
+	private String outputStreamName;
+	
+    public Dispatcher getDispatcher() {
+        return dispatcher;
+    }
+
+    public void setDispatcher(Dispatcher dispatcher) {
+        this.dispatcher = dispatcher;
+    }
+
+    public String getOutputStreamName() {
+        return outputStreamName;
+    }
+
+    public void setOutputStreamName(String outputStreamName) {
+        this.outputStreamName = outputStreamName;
+    }	
+	
     public void processEvent(Sentence sentence) {
-        System.out.printf("Sentence is '%s', location %s\n", sentence.getText(), sentence.getLocation());
+    	System.out.printf ("Id: %d\nSpeechid: %d\nText:%s\nTime: %d\nLocaltion: %s\n",
+    			sentence.getId(), sentence.getSpeechId(), sentence.getText(),
+    			sentence.getTime(), sentence.getLocation());
+    	
+    	Sentence sent2 = new Sentence();
+    	sent2.setId(sentence.getId());
+    	sent2.setText ("Look, ma, I can dispatch events: " + sentence.getText());
+    	sent2.setTime (sentence.getTime());
+    	
+    	if (this.dispatcher != null)
+    		System.out.println("Dispatcher is not null");
+    	
+    	System.out.println("Out stream name: " + this.outputStreamName);
+    	
+//        dispatcher.dispatchEvent(outputStreamName, sent2);
     }
     
     @Override
